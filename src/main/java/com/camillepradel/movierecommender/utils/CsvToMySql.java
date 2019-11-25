e5dfea80bead2281e154dc8df7d44e9288f584c5
DenisGuiraudet <guiraudet@live.fr>
2019-11-25 15:19:51 +0100

    Update csv path

--- src/main/java/com/camillepradel/movierecommender/utils/CsvToMySql.java
+++ src/main/java/com/camillepradel/movierecommender/utils/CsvToMySql.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 
 public class CsvToMySql {
 
-    static final String pathToCsvFiles = "D:\\MovieRecommender\\src\\main\\java\\com\\camillepradel\\movierecommender\\utils\\";
+    static final String pathToCsvFiles = "/home/denis/repo/ICE_MovieRecommender/src/main/java/com/camillepradel/movierecommender/utils/";
     static final String usersCsvFile = pathToCsvFiles + "users.csv";
     static final String moviesCsvFile = pathToCsvFiles + "movies.csv";
     static final String genresCsvFile = pathToCsvFiles + "genres.csv";
@@ -392,10 +392,10 @@ public class CsvToMySql {
         }
 
         // db connection info
-        String url = "jdbc:mysql://localhost:3306"
-                + "?zeroDateTimeBehavior=convertToNull&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://localhost:3306";
+                //+ "?zeroDateTimeBehavior=convertToNull&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         String login = "admin";
-        String password = "Q86PhnJRiEa7";
+        String password = "6ExgfeNs0dbi";
 
         Connection connection = null;
 
